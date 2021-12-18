b9ebd021d8ae3a593c224d33e31801135bfde193
aghArdeshir <ardeshireo@gmail.com>
2021-12-18 19:58:50 +0400

    folder restructure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './frontend/App';
+import reportWebVitals from './frontend/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
