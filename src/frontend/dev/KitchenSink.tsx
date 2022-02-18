da13fb27cbab25cdc7a9ad4ead630ec5ad6a2dee
aghArdeshir <ardeshireo@gmail.com>
2022-02-18 23:30:53 +0400

    add another item and cart to kitchen sink

--- src/frontend/dev/KitchenSink.tsx
+++ src/frontend/dev/KitchenSink.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Cart from "../components/cart/Cart";
 import Item from "../components/item/Item";
 
 const thingsToDemo: [string, JSX.Element][] = [
   [
-    "ITEM",
+    "ITEM WITH ID 1",
     <Item
       id={1}
       description="Some very very very long description"
@@ -12,21 +12,29 @@ const thingsToDemo: [string, JSX.Element][] = [
       title="Some Title"
     />,
   ],
+  [
+    "ITEM WITH ID 2",
+    <Item
+      id={2}
+      description="This is another item"
+      imageSrc="https://via.placeholder.com/150"
+      price={4500}
+      title="The Second Item"
+    />,
+  ],
+  ["CART", <Cart />],
 ];
 
 export default function KitchenSink() {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "row", gap: 8 }}>
       {thingsToDemo.map(([name, thing]) => (
-        <React.Fragment key={name}>
+        <div key={name}>
           {name}
           <br />
           {thing}
-          <hr />
-          <hr />
-          <hr />
-        </React.Fragment>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
